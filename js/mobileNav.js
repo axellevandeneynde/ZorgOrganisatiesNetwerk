51daf0fb23e51bb8aab029dd048bed24d74e9769
Axelle Vanden Eynde <axelle.vanden.eynde@student.ehb.be>
2019-08-30 14:56:09 +0200

    mobile versie verbeterd
    overal dezelfde waardes in de media-queries en js, ales overal op zijn plaats gezet op het grid. navigatie aangepast ook bij alle individuele organisaties.

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -5,26 +5,16 @@ $("#mobileMenuIcon").click(function(){
 	// De navigatie tevoorschijn laten komen
 	$(nav).slideToggle();
 	// Veranderen van hamburger icon naar een kruisje
-	$(".mobileIconLijn1").toggleClass('knopAnimatie1');
-	$(".mobileIconLijn2").toggleClass('knopAnimatie2');
-	$(".mobileIconLijn3").toggleClass('knopAnimatie3');
+
 });
 // Fix voor het verdwijnen van de navigatie bij resize van schermbreedte
 $(window).on('resize', function(){
-		//Bij resizen van scherm de hamburger icon resetten
-		$(".mobileIconLijn1").removeClass('knopAnimatie1');
-		$(".mobileIconLijn2").removeClass('knopAnimatie2');
-		$(".mobileIconLijn3").removeClass('knopAnimatie3');
-	// Bij schermen onder de 890px 
-	if($(this).width() <= 890){
+	// Bij schermen onder de 1024px 
+	if($(this).width() <= 1193){
 		$("#mobileNav").css("display", "none");
 	}
-	// Bij schermen groter dan 890px 
-	if($(this).width() > 890){
+	// Bij schermen groter dan 1024px 
+	if($(this).width() > 1193){
 		$("#mobileNav").css("display", "inline-block");
-		// Fix voor kruisje dat blijft staan wanneer die weer een hamburger icon moet zijn
-		$(".mobileIconLijn1").removeClass('knopAnimatie1');
-		$(".mobileIconLijn2").removeClass('knopAnimatie2');
-		$(".mobileIconLijn3").removeClass('knopAnimatie3');
 	};
 });
