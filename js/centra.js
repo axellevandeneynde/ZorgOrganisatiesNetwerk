272bd4a27649f8b81d8a4f0a82f1d41ba61d3d6d
Julien Menten <julien.menten@student.ehb.Be>
2019-09-18 13:10:42 +0200

    Admin begin
    Admin folder aangemaakt
    json file met centra aangemaakt
    aparte folders per centra gemaakt in images

---
+++ js/centra.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    console.log("linked!");
+
+    $.ajax({
+        url: '../json/centra.json',
+        dataType: 'JSON',
+        method: 'GET'
+    }).done(function(data){
+        console.log(data);
+        var centra_ID = data.centra[0].centra_ID;
+        $("#testDiv").html(
+           '<img src="../images/centra/'+ centra_ID +'/logo.png" alt="">'
+        );
+    }).fail(function(a,b){
+        console.log(a,b);
+    }).always(function(){
+        console.log("Julien is the best!");
+    });
+});
