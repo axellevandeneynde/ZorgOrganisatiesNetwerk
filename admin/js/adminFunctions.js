e34657db89f2d023497f3750b0277a0379699a3e
Julien Menten <julien.menten@student.ehb.Be>
2019-10-06 16:29:51 +0200

    Admin

--- admin/js/adminFunctions.js
+++ admin/js/adminFunctions.js
@@ -1,33 +1,38 @@
+'use strict'
 $(document).ready(function(){
     console.log("linked!");
     voegCentrumToe();
     toonCentra();
 });
-
+//! Centrum toevoegen aan lijst
 function voegCentrumToe(){
     $("#centrumForm").submit(function(e){
         e.preventDefault();
-        var formResult = $(this).serializeArray();
-        console.log(formResult);
         
-        var centrumObject = {
-            
+        let centrumObject = {
+            naam: $("#naamInput").val()
         };
 
         // POST methode
         $.ajax({
-            url: "http://mentenjulien.com/insertCentrum",
-            data: centrumObject,
-            method:'POST'
+            url: "http://127.0.0.1:3000/insertCentrum",
+            method:'POST',
+            data: centrumObject
         }).done(function(data){
             console.log(data);
+            console.log('Centrum toegevoegd!')
+        }).fail(function(er1, er2){
+            console.log(er1);
+            console.log(er2);
         });
     });
 }
 
+
+//! Vertonen van lijst van centra en die aanpassen
 function toonCentra(){
     $.ajax({
-        url: '../json/centra.json',
+        url: 'http://127.0.0.1:3000/getCentra',
         method: 'GET',
         dataType: 'JSON'
     }).done(function(data){
@@ -35,22 +40,29 @@ function toonCentra(){
         var centraLijst = data.centra; 
         // ! for-OF loop voor objecten van in een json file te lezen!
         for (var centrum of centraLijst){
-            $("#centraLijst").append();
+            $("#centraLijst").append("<li>"+ maakCentrumAan(centrum));
         }
+    }).fail(function(er1, er2){
+        console.log(er1, er2)
     });
 }
 
-function maakCentraAan(centrum_object){
-    centrumPrintObject = {
+function maakCentrumAan(centrum_object){
+    var naam = centraObjectAanmaken(centrum_object).centrumNaam; 
+    var doelgroepen = centraObjectAanmaken(centrum_object).centrumDoelgroepen;
+    return "<div>" + naam + doelgroepen;
+}
+function centraObjectAanmaken(centrum_object){
+    let centrumPrintObject = {
         centrumNaam: "<h2>"+centrum_object.naam+"</h2>",
-        centrumDoelgroepen: "<ul class='doelgroepenList' id='centrum"+centrum_object.centra_ID+"'>"+ makeListFromArray('#centrum'+centrum_object.centra_ID , centrum_object.doelgroep)
+        centrumDoelgroepen: "<ul class='doelgroepenList' id='centrum"+centrum_object.centra_ID+"'>"+ makeListFromArray('#centrum'+centrum_object.centra_ID, centrum_object.doelgroep) + "</ul>"
     };
     return centrumPrintObject;
 }
 
 function makeListFromArray(list, array){
-    for(var index of array){
-        $(list).append("<li>"+ index +"</li>");
-        console.log("<li>"+index+"</li>");
+    for(let index of array){
+        let li = "<li>" + index + "</li>"
+        $(list).append(li);
     }
 }
--- node/server.js
+++ node/server.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const fs = require('fs'); //! install fs with node!
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 const port = 3000;
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => 
-    es.send('Hello World!')
+    res.send('Hello World!')
 );
 
 app.listen(port, () => 
     console.log(`Example app listening on port ${port}!`)
 );
+
+app.get('/getCentra', (req, res) => 
+fs.readFile('json/centra.json', 'utf8', function(error, data){
+    res.json(JSON.parse(data));
+}));
+
+app.post('/insertCentrum', (req, res) => {
+    console.log('insert triggered!');
+    var data = JSON.stringify(req.body);
+    var file = JSON.parse(fs.readFileSync('json/centra.json', 'utf8'));
+    console.log(data, file);
+	var newProjectID = req.body.project_ID;
+    
+    var arr = file.projects;
+    var dataString = JSON.parse(data);
+    arr.push(dataString);
+    file.projects = arr;
+    
+    fs.writeFile('json/centra.json', JSON.stringify(file), function(err){
+       if (err) throw err;
+    });
+	
+    res.send('OK');
+});
