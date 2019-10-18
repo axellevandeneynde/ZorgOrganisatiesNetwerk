3748675fc28be9b73e545831791862ac11e99aaa
Julien Menten <julien.menten@student.ehb.Be>
2019-10-18 09:28:28 +0200

    Lijst weergeven van alle centra vanuit een json file-> werkt

--- js/Centrum.js
+++ js/Centrum.js
@@ -1,7 +1,7 @@
 'use strict';
-class Centrum{
-    constructor(){
-        this.id = generateID();
+export default class Centrum{
+    constructor(name, beschrijving, website, email, telefoonnummer, adres, regio){
+        this.id = this.generateID();
         this.name = name; 
         this.doelgroep = [];
         this.leeftijdsgroep = [];
@@ -13,7 +13,7 @@ class Centrum{
         this.regio = regio;
     }
     generateID(){
-
+        return "69";
     }
 
     createCentrumPage(){
@@ -21,13 +21,13 @@ class Centrum{
     }
     createCentrumThumbnail(){
         return `<div class="organisatie all ${this.doelgroep} all ${this.leeftijdsgroep} all ${this.regio}">
-                    <figure><img src="" alt="logo ${this.naam}"></figure>
+                    <figure><img src="" alt="logo ${this.name}"></figure>
                     <article class="org_tekst">
                         <h4>${this.name}</h4>
                         <p>${this.beschrijving}</p> 
                         <div class="org_buttons">
-                            <a href="https://${this.website}" target="_blank>Website</a>
-                            <a href="centrum.html/">Meer informatie <span>→</span></a>
+                            <a href="https://${this.website}" target="_blank">Website</a>
+                            <a href="centrum.html:${this.name}">Meer informatie <span>→</span></a>
                         </div>
                     </article>
                 </div>`;
--- js/centra.js
+++ js/centra.js
@@ -1,17 +1,27 @@
+import Centrum from './Centrum.js';
+
 $(document).ready(function(){
     console.log("linked!");
 
     $.ajax({
-        url: '../json/centra.json',
+        url: 'http://localhost:3000/getCentra',
         dataType: 'JSON',
         method: 'GET'
     }).done(function(data){
-        console.log(data);
-        var centra_ID = data.centra[0].centra_ID;
-        $("#testDiv").html(
-           '<img src="../images/centra/'+ centra_ID +'/logo.png" alt="">'
-        );
-        voegCentrumToe();
+        let centraArray = data.centra;
+        console.log(centraArray);
+        for(let centrum of centraArray){
+            let centrumName = centrum.naam;
+            let centrumBeschrijving = centrum.beschrijving;
+            let centrumWebsite = centrum.website;
+            let centrumEmail = centrum.email;
+            let centrumTel = centrum.telefoonnummer;
+            let centrumAdres = centrum.adres;
+            let centrumRegio= centrum.regio;
+            let newcentrum = new Centrum(centrumName, /*centrumDoelgroep, centrumLeeftijdsgroep,*/ centrumBeschrijving, centrumWebsite, centrumEmail, centrumTel, centrumAdres, centrumRegio);
+            $("#testDiv").append(newcentrum.createCentrumThumbnail());
+        }
+       
     }).fail(function(a,b){
         console.log(a,b);
     }).always(function(){
