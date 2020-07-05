37771da136418a3b6a3856028639e14ebb722fc5
julienmenten <43605864+julienmenten@users.noreply.github.com>
2020-07-05 03:30:39 +0200

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
-const port =  process.env.port || 8000;
+const port =  process.env.PORT || 8000;
 const url = "localhost" + "/";
 
 
